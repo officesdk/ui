4b57830bf998b00c48fed46f8e6a59481f28a0d1
byte9527 <1026859692@qq.com>
2025-12-31 15:27:22 +0800

    fix: fix type error for theme

--- packages/theme/tsup.config.ts
+++ packages/theme/tsup.config.ts
@@ -3,17 +3,11 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  dts: false,
+  dts: true,
   clean: true,
   sourcemap: true,
-  external: [
-    'react',
-    'react-dom',
-    'styled-components',
-    'react/jsx-runtime',
-  ],
+  external: ['react', 'react-dom', 'styled-components', 'react/jsx-runtime'],
   treeshake: true,
   splitting: false,
   minify: false,
 });
-
