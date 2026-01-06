3afdb9981fa4ec097f7f41263d471bc2c29ca564
zhangmeng <zhangmeng@git.shimo.im>
2026-01-06 23:21:17 +0800

    chore: fix type error

--- packages/components/src/UIConfigProvider/UIConfigProvider.tsx
+++ packages/components/src/UIConfigProvider/UIConfigProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect } from 'react';
+import React, { createContext, useContext } from 'react';
 import ReactDOM from 'react-dom';
 import { IconProvider } from '../Icon/IconProvider';
 import { ToastContainer } from '../Toast/ToastContainer';
